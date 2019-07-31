53ed56f6c821ebcddce1e32399e890dea049ee84
nayemalam <nayemalam@hotmail.com>
2019-07-31 11:13:40 +0800

    almost completed - need all-screen responsiveness now - added favicon - added hero header - changed styling - all math completed and conditions met for the actual core of the project - minimal tests have been done - pushed to github.io

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,41 +14,37 @@ var submit = document.querySelector(".submit");
 var reset = document.querySelector(".reset");
 
 // only when genders are changed
-sex.addEventListener("input", function() {
-    if(sex.value == "male") {
-        console.log("I am male");
-    }
-    if(sex.value == "female") {
-        console.log("I am female");
-    }
-});
+// sex.addEventListener("input", function() {
+//     if(sex.value == "male") {
+//         console.log("I am male");
+//     }
+//     if(sex.value == "female") {
+//         console.log("I am female");
+//     }
+// });
 
-weightType.addEventListener("input", function() {
-    if(weightType.value == "kg") {
-        console.log("I chose kg");
-        console.log(weightInput.value);
-    }
-    if(weightType.value == "lbs") {
-        console.log("I chose pounds");
-        console.log(weightInput.value);
-    }
-});
+// weightType.addEventListener("input", function() {
+//     if(weightType.value == "kg") {
+//         console.log("I chose kg");
+//         console.log(weightInput.value);
+//     }
+//     if(weightType.value == "lbs") {
+//         console.log("I chose pounds");
+//         console.log(weightInput.value);
+//     }
+// });
 
-heightType.addEventListener("input", function() {
-    if(heightType.value == "kg") {
-        console.log("I chose kg");
-        console.log(heightInput.value);
-    }
-    if(heightType.value == "lbs") {
-        console.log("I chose pounds");
-        console.log(heightInput.value);
-    }
-});
-
-
+// heightType.addEventListener("input", function() {
+//     if(heightType.value == "kg") {
+//         console.log("I chose kg");
+//     }
+//     if(heightType.value == "lbs") {
+//         console.log("I chose pounds");
+//     }
+// });
 
 submit.addEventListener("click", function() {
-    var total = 0;
+    var total = 0, weight = 0, height = 0;
     // activity factors from: https://www.exerciselifestyle.uk/nutrition/balancing-your-diet/
     if(activityType.value == "sedentary") {
         console.log("I am sedentary");
@@ -66,29 +62,37 @@ submit.addEventListener("click", function() {
         console.log("I am highly-active");
         multiplier = 1.9;
     }
+
+    if(weightType.value == "lbs") {
+        weight = weightInput.value*0.453592;
+    }
+    if(heightType.value == "inches") {
+        height = heightInput.value*2.54;
+    }
     
     if(sex.value == "male") {
-        total = 10 * weightInput.value + 6.25 * heightInput.value - 5*age.value + 5;
-        bmr.textContent = total;
-        bmr.style.fontSize = "25px";
-        calories.textContent = total * multiplier + "/day";
-        calories.style.fontSize = "50px";
-        console.log(total);
-        console.log(multiplier);
-    } else if (isNaN(weightInput.value) || isNaN(heightInput.value)) {
+        console.log("I am male");
+        total = Math.round(10 * weight + 6.25 * height - 5*age.value + 5);
+        bmr.textContent = total, bmr.style.fontSize = "25px";
+        calories.textContent = total * multiplier + "/day", calories.style.fontSize = "50px";
+    } 
+    if(sex.value == "female") {
+        console.log("I am female");
+        total = Math.round(10 * weight + 6.25 * height - 5*age.value - 161);
+        bmr.textContent = total, bmr.style.fontSize = "25px";
+        calories.textContent = total * multiplier + "/day", calories.style.fontSize = "50px";
+    }
+    else if (isNaN(weightInput.value) || isNaN(heightInput.value)) {
         alert("Something wrong with the code, please contact: github/nayemalam")
     } 
-    // console.log(total);
-    // bmr.textContent = total;
-    // console.log(weightType.value);
-    // console.log(heightType.value);
-    // console.log(age.value);
-    // console.log(activityType.value);
-    
 });
 
 reset.addEventListener("click", function() {
-
-})
+    sex.value = "male";
+    weightType.value = "kg";
+    heightType.value = "cm";
+    activityType.value = "sedentary";
+    console.log("reset");
+});
 
 
